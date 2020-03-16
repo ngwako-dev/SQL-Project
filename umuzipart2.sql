d3ccd4b2cd4106397cd01e7a9005744d99a74da7
ngwako-dev <56994932+ngwako-dev@users.noreply.github.com>
2020-03-16 17:42:05 +0200

    Add files via upload

---
+++ umuzipart2.sql
@@ -0,0 +1,11 @@
+1. SELECT * from customers;
+
+2. SELECT firstname
+   from customers;
+
+3. SELECT firstname
+   FROM customers
+   WHERE id=1
+
+4. UPDATE customers SET firstname='Lerato',lastname='Mabitso'
+   WHERE id=1 
