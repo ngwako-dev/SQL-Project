3a64997b057b369fe3d929cb30c8787eca91ebd8
ngwako-dev <56994932+ngwako-dev@users.noreply.github.com>
2020-03-17 22:53:11 +0200

    Update umuzipart2.sql

--- umuzipart2.sql
+++ umuzipart2.sql
@@ -1,11 +1,70 @@
-1. SELECT * from customers;
+1. SELECT * FROM "Customers Table";
 
-2. SELECT firstname
-   from customers;
+2. SELECT "FirstName"
+   FROM "Customers Table";
 
-3. SELECT firstname
-   FROM customers
-   WHERE id=1
+3. SELECT "FirstName"
+   FROM "Customers Table"
+   WHERE "CustomerId"=1;
 
-4. UPDATE customers SET firstname='Lerato',lastname='Mabitso'
-   where id=1 
+4. UPDATE "Customers Table" SET "FirstName"='Lerato',"LastName"='Mabitso'
+   WHERE "CustomerId"=1;
+
+5. DELETE FROM "Customers Table" WHERE "CustomerId"=2;
+
+6. SELECT DISTINCT ("Status")
+   FROM "Orders Table";
+
+   SELECT COUNT (DISTINCT "Status")
+   FROM "Orders Table";
+
+7. SELECT MAX("Amount") FROM "Payments Table";
+
+8. SELECT * FROM "Customers Table"
+   ORDER BY "Country";
+
+9. SELECT * FROM "Products Table"
+   WHERE "BuyPrice" BETWEEN 100 AND 600;
+
+10. SELECT * FROM "Customers Table"
+    WHERE "Country"='Germany' AND "City"='Berlin';
+
+11. SELECT * FROM "Customers Table"
+    WHERE "City"='Cape Town' OR "City"='Durban';
+
+12. SELECT * FROM "Products Table"
+    WHERE "BuyPrice" >500;
+
+13. SELECT SUM("Amount")
+    FROM "Payments Table";
+
+14. SELECT COUNT("Status")
+    FROM "Orders Table"
+    WHERE "Status"='Shipped'
+
+15. SELECT AVG("BuyPrice")
+    FROM "Products Table";
+
+    SELECT AVG("BuyPrice"/12)
+    FROM "Products Table"
+
+16. SELECT
+    "Customers Table"."CustomerId",
+    "FirstName",
+    "LastName",
+    "Gender",
+    "Address",
+    "Phone",
+    "Email",
+    "City",
+    "Country"
+    FROM
+   "Customers Table"
+   INNER JOIN "Payments Table" ON "Payments Table"."CustomerId" = "Customers Table"."CustomerId"
+   ORDER BY
+   "Customers Table"."CustomerId";
+
+
+17. SELECT "ProductName"
+    FROM "Products Table"
+    WHERE "Description"='Turnable front wheels, steering function';
